ba155dbb49a56b8eb41472c69edac01c8e7f7272
8Aegislash8 <naufalchannel303@gmail.com>
2025-11-08 14:58:49 +0700

    Create_Coding_bilangan_genap_atau_ganjil.cpp
    Hehehe

---
+++ Coding_bilangan_genap_atau_ganjil.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int angka;
+	cout << "-------Penentuh bilangan ganjil genap-------" << endl;
+	cout << "Masukan bilangan" << endl;
+	cin >> angka;
+	if (angka % 2 == 0) {
+		cout << "Bilangan genap" << endl;
+	}
+		 else {cout << "Bilangan ganjil" << endl;
+		 }
+}
